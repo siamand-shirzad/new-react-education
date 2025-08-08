4fed91110359c770c21932065b3dcb0ecfadccf6
siamand <siamand1381@gmail.com>
2025-08-08 15:35:00 +0330

    inputForm_useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,53 @@
-import {  useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
-	const [count, setCount] = useState(0);
-	let countValue = useRef(0);
-	const handleClick = () => {
-		countValue.current += 2;
-		setCount(countValue.current + 1);
+	const [inputValue1, setInputValue1] = useState("");
+	const [inputValue2, setInputValue2] = useState("");
+	const inputRef1 = useRef(null);
+	const inputRef2 = useRef(null);
+	useEffect(() => {
+		inputRef1.current.focus();
+	}, []);
+	const handleKeyDown = (e) =>{
+		if (e.key === "Enter") {
+			inputRef2.current.focus();
+		}
 	}
+
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-purple-500 to-pink-500">
-			<div className="flex flex-col text-white items-center p-8 rounded-lg shadow-lg space-y-6 bg-white">
-				<h2 className="text-gray-700 font-semibold text-3xl">counter app</h2>
-				<div className="flex items-center justify-center rounded-full bg-gradient-to-r from-blue-400 to-indigo-500 w-36 h-36">
-					<span className="text-4xl text-white font-bold">{count}</span>
+		<div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-teal-400 to-indigo-600">
+			<div className="text-center p-8 rounded-lg shadow-2xl space-y-6 bg-white w-full max-w-md">
+				<h2 className="text-gray-700 font-semibold text-3xl"> فرم ورودی</h2>
+
+				<div className="relative" >
+					<input type="text " className="w-full  px-4 py-3 text-sm text-center border-2 border-teal-300 focus:ring-4 focus:ring-teal-200
+					focus:border-teal-500 outline-none transition duration-300 bg-gray-50 rounded "
+						placeholder="متن خود را وارد کنید..."
+						value={inputValue1}
+						onChange={(e) => setInputValue1(e.target.value)}
+						ref={inputRef1} 
+						onKeyDown={handleKeyDown}/>
+					<input type="text " className="w-full mt-4 px-4 py-3 text-sm text-center border-2 border-teal-300 focus:ring-4 focus:ring-teal-200
+					focus:border-teal-500 outline-none transition duration-300 bg-gray-50 rounded "
+						placeholder="متن خود را وارد کنید..."
+						value={inputValue2}
+						onChange={(e) => setInputValue2(e.target.value)}
+						ref={inputRef2} />
 				</div>
-				<button onClick={handleClick}
-					className="px-6 py-4 bg-gradient-to-r from-green-400 to-blue-500 rounded-lg cursor-pointer font-bold
-				hover:shadow-2xl transform hover:scale-105 transition duration-300 ease-in-out focus:ring-2 ring-blue-300 focus:outline-none
-				">افزایش </button>
+				{inputValue1 && (
+					<div className="mt-4 p-4 bg-teal-50 rounded border border-teal-300">
+						<p className="text-teal-800 text-center" >متن وارد شده: <span className="font-bold">{`${inputValue1} ${inputValue2}`}</span></p>
+					</div>
+				)}
+
+				<div className="flex justify-center gap-4 space-x-4 ">
+					<button onClick={() => {setInputValue1("") ,setInputValue2("")}} className="px-4 py-2 bg-teal-600 text-white font-medium rounded shadow-md hover:bg-teal-700 transition duration-300 
+				focus:outline-none focus:ring-2 focus:ring-teal-500">پاک کردن</button>
+					<button className="px-4 py-2 bg-indigo-600 text-white font-medium rounded shadow-md hover:bg-indigo-700 transition duration-300 
+				focus:outline-none focus:ring-2 focus:ring-indigo-500">ارسال</button>
+				</div>
 
 			</div>
-
-
-
 		</div>
 
 	);
